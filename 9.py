acbec5c197580ccafc1db666a4aba976f1d92a48
Don98 <chenqd6@mail2.sysu.edu.cn>
2022-06-14 01:35:48 +0800

    delete no needed

--- 9.py
+++
@@ -1,6 +0,0 @@
-import re
-# regexkeywords = r"(?<=\s)" + "return" + r"(?=\s)"
-regexkeywords = r"(?<=\s)return(.*)"
-a = "\treturn; a"
-a = re.sub(regexkeywords, " [key] " + "return;" + " [end] ", a)
-print(a)
